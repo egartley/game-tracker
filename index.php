b09eed71d263d395dede4f57ab14fe62f11fdea0
Evan Gartley <evanmgartley@gmail.com>
2023-09-16 18:10:33 -0400

    Update files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Game Tracker</title>
     <?php
-        require_once 'includes/min-header.php'
+        require_once 'includes/header.php'
     ?>
 </head>
 
@@ -15,122 +15,74 @@
         </div>
         <div class="game-list unified-container compact">
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
                     <div class="game-title">Game Title 1</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
                     <div class="game-title">Game Title 2</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
                     <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 4</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 5</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 6</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 7</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 8</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
             <div class="game-listing compact">
-                <div class="game-icon"></div>
+                <div class="game-icon"><img src="/resources/png/default-icon.png"></div>
                 <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
-                    <div class="subtext">20XX, Platform</div>
-                </div>
-            </div>
-            <div class="game-listing compact">
-                <div class="game-icon"></div>
-                <div class="game-details">
-                    <div class="game-title">Game Title 3</div>
-                    <div class="rating">[X] [X] [X] [X] [X]</div>
+                    <div class="game-title">Game Title 9</div>
+                    <div class="rating"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sf.png"><img src="/resources/png/sh.png"><img src="/resources/png/se.png"></div>
                     <div class="subtext">20XX, Platform</div>
                 </div>
             </div>
